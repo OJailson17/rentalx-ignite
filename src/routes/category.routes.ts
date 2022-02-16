6aa3a66d5bd3f859dc544061d41f52e8ff8c6514
OJailson17 <JaylsonO17@gmail.com>
2022-02-15 23:45:15 -0300

    insert category types

---
+++ src/routes/category.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { Category } from '../model/Category';
+
+const CategoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+CategoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return res.status(201).json(categories);
+});
+
+export { CategoriesRoutes };
