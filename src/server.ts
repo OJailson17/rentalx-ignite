07006132017929752ac1fadd03e5d996d55430ba
OJailson17 <JaylsonO17@gmail.com>
2022-02-15 22:34:27 -0300

    create server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
+app.listen(8082, () => console.log('Server is running'));
