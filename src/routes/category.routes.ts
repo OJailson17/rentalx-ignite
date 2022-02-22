9e0880f5385b730ed334b1fe45d4ce2600ec770b
OJailson17 <JaylsonO17@gmail.com>
2022-02-22 04:04:26 -0300

    feat: add file import feature

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCase/createCategory';
 import { listCategoriesController } from '../modules/cars/useCase/listCategories';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (req, res) => createCategoryController.handle(req, res));
 
 categoriesRoutes.get('/', (req, res) => listCategoriesController.handle(req, res));
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  const { file } = req;
+
+  console.log(file);
+
+  return res.send();
+});
 
 export { categoriesRoutes };
