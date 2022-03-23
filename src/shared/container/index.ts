7569a37af1536ec8c64baf6387f2c315ece790f8
OJailson17 <JaylsonO17@gmail.com>
2022-03-22 23:28:45 -0300

    add users repository to dependencies container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUserRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
@@ -11,3 +13,5 @@ container.registerSingleton<ISpecificationRepository>(
   'SpecificationRepository',
   SpecificationRepository,
 );
+
+container.registerSingleton<IUserRepository>('UsersRepository', UsersRepository);
