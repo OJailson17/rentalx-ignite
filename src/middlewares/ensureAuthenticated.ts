fb2e3c61877a802169b18e313fbc407cc4589076
OJailson17 <JaylsonO17@gmail.com>
2022-03-25 23:16:09 -0300

    add token validation

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
 export const ensureAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
@@ -11,8 +13,14 @@ export const ensureAuthenticated = async (req: Request, res: Response, next: Nex
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub } = verify(token, process.env.JWT_SECRET);
-    console.log(sub);
+    const { sub: user_id } = verify(token, process.env.JWT_SECRET);
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(String(user_id));
+
+    if (!user) {
+      throw new Error('User does not exists');
+    }
 
     next();
   } catch {
