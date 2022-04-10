7dad04c7fdf50ae45bb69624c8fa982343d0447f
OJailson17 <JaylsonO17@gmail.com>
2022-04-10 20:38:55 -0300

    create test for create category validation

--- src/modules/cars/useCase/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCase/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../errors/AppError';
 import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
@@ -20,8 +21,19 @@ describe('Create Category', () => {
 
     const createdCategory = await categoriesRepositoryInMemory.findByName(category.name);
 
-    console.log(createdCategory);
-
     expect(createdCategory).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category with the same name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description',
+      };
+
+      await createCategoryUseCase.execute(category);
+
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
