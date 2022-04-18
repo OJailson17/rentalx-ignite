f9ca1d5732f5d9c3ee326ac7414c97cb9edfb0dd
OJailson17 <JaylsonO17@gmail.com>
2022-04-17 22:55:27 -0300

    feat: add create cars feat using typeorm

--- src/modules/cars/useCase/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCase/createCar/CreateCarUseCase.ts
@@ -14,10 +14,10 @@ interface IRequest {
   category_id: string;
 }
 
-// @injectable()
+@injectable()
 class CreateCarUseCase {
   constructor(
-    // @inject('CarRepository')
+    @inject('CarRepository')
     private carsRepository: ICarsRepository,
   ) {}
 
