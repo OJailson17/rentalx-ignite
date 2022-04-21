0c56cebd0f1fa99bb5a6f6a001c2f229a3ac3961
OJailson17 <JaylsonO17@gmail.com>
2022-04-21 00:11:30 -0300

    create list cars route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCase/createCar/CreateCarController';
+import { ListCarsController } from '@modules/cars/useCase/listCars/ListCarsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -8,7 +9,9 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listCarsController = new ListCarsController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+carsRoutes.get('/available', listCarsController.handle);
 
 export { carsRoutes };
