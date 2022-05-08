d5cc63974b1b3ab6fddb65cea1f69c415d4aa217
OJailson17 <JaylsonO17@gmail.com>
2022-05-08 20:07:29 -0300

    create rentals migration

--- src/shared/infra/typeorm/migrations/1647991520055-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1647991520055-CreateUsers.ts
@@ -10,6 +10,7 @@ export class CreateUsers1647991520055 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
