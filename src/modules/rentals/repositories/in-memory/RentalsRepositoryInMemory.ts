010b0facc1e2eacf6e173b38e6a60e78bfe76b8c
OJailson17 <JaylsonO17@gmail.com>
2022-05-09 21:24:16 -0300

    add create rental initial test

---
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -0,0 +1,13 @@
+import { Rental } from '../../infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '../IRentalRepository';
+
+export class RentalsRepositoryInMemory implements IRentalsRepository {
+  rentals: Rental[] = [];
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.rentals.find((rental) => rental.car_id === car_id && rental.end_date === null);
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.rentals.find((rental) => rental.user_id === user_id && rental.end_date === null);
+  }
+}
