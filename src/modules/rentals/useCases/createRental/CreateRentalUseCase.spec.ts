330e77437b660e547e0f8da83e1d3cfa959e4ff5
OJailson17 <JaylsonO17@gmail.com>
2022-05-10 21:18:23 -0300

    create test to test rental creation

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
@@ -12,10 +13,29 @@ describe('Create Rental', () => {
   });
 
   it('should be able to create a new rental', async () => {
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       car_id: '121212',
       user_id: '123456',
       expected_return_date: new Date(),
     });
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+
+  it('should not be able to create a new rental if the user has an open rent', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        car_id: '121212',
+        user_id: '123456',
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        car_id: '121212',
+        user_id: '123456',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
