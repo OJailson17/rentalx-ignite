5bf4e1db15a72034fb5027a4b267f582030d07d8
OJailson17 <JaylsonO17@gmail.com>
2022-05-14 00:14:43 -0300

    make an dependency injection

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
@@ -9,8 +11,14 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 export class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository, private dateProvider: IDateProvider) {}
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
 
   async execute({ car_id, user_id, expected_return_date }: IRequest): Promise<Rental> {
     const minHours = 24;
