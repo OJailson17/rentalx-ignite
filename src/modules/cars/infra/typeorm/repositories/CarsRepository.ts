7e9b404a85b7d27e24876e61f3a5466298feeaf4
OJailson17 <JaylsonO17@gmail.com>
2022-05-23 03:13:56 -0300

    add update status car method

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -72,4 +72,13 @@ export class CarsRepository implements ICarsRepository {
 
     return car;
   }
+
+  async updateAvailable(id: string, available: boolean): Promise<void> {
+    await this.repository
+      .createQueryBuilder()
+      .update()
+      .set({ available })
+      .where('id = :id', { id })
+      .execute();
+  }
 }
