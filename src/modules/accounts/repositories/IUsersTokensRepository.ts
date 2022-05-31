04c61ab932e42d28d904bd5dc49cf7ae050fcdb6
OJailson17 <JaylsonO17@gmail.com>
2022-05-31 10:23:03 -0300

    add find and delete method

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -3,4 +3,6 @@ import { UserTokens } from '../infra/typeorm/entities/UserTokens';
 
 export interface IUsersTokensRepository {
   create({ user_id, expires_date, refresh_token }: ICreateUsersTokenDTO): Promise<UserTokens>;
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
+  deleteById(id: string): Promise<void>;
 }
