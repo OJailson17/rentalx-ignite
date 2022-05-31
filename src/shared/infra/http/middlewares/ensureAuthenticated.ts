8f96ef976e7c7eb100461d16060df46e04632736
OJailson17 <JaylsonO17@gmail.com>
2022-05-31 20:46:37 -0300

    add refresh token route

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import auth from '@config/auth';
+// import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UserTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 export const ensureAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
+
+  const usersTokenRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError('Token missing', 401);
@@ -14,17 +18,17 @@ export const ensureAuthenticated = async (req: Request, res: Response, next: Nex
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(token, process.env.JWT_SECRET);
+    const { sub: user_id } = verify(token, auth.secret_refresh_token);
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(String(user_id));
+    // const usersRepository = new UsersRepository();
+    const user = await usersTokenRepository.findByUserIdAndRefreshToken(String(user_id), token);
 
     if (!user) {
       throw new AppError('User does not exists', 401);
     }
 
     req.user = {
-      id: user.id,
+      id: String(user_id),
     };
 
     next();
