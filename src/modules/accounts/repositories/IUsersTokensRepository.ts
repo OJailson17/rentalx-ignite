651d0cb945320d5821bc666bee5f914ae6d7414d
OJailson17 <JaylsonO17@gmail.com>
2022-06-03 00:07:44 -0300

    add method to get refresh token

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -5,4 +5,5 @@ export interface IUsersTokensRepository {
   create({ user_id, expires_date, refresh_token }: ICreateUsersTokenDTO): Promise<UserTokens>;
   findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
   deleteById(id: string): Promise<void>;
+  findByRefreshToken(refresh_token: string): Promise<UserTokens>;
 }
