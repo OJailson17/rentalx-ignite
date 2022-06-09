4cf0e83faf56b9a3f877b57b553471b870f7e633
OJailson17 <JaylsonO17@gmail.com>
2022-06-09 02:08:06 -0300

    create storage providers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,16 @@ import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
+import { LocalStorageProvider } from './StorageProvider/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/S3StorageProvider';
 
 container.registerSingleton<IDateProvider>('DayjsDateProvider', DayjsDateProvider);
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider());
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>('StorageProvider', diskStorage[process.env.DISK]);
